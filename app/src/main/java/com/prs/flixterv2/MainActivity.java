1afbc94feae88da677c5e7937b052ddc57df0ed3
ralph <placideralph@gmail.com>
2019-11-04 19:41:40 -0500

    Working on UI

--- app/src/main/java/com/prs/flixterv2/MainActivity.java
+++ app/src/main/java/com/prs/flixterv2/MainActivity.java
@@ -5,8 +5,12 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 
 import com.prs.flixterv2.Fragments.MoviesFragment;
@@ -33,8 +37,25 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         setTitle(getResources().getString(R.string.app_name));
 
+        spCategory.setLayoutParams(new Toolbar.LayoutParams(300, ViewGroup.LayoutParams.WRAP_CONTENT));
+
         String[] entries = new String[]{"Movies", "TV Shows"};
-        spCategory.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item, entries));
+        spCategory.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, entries));
+        spCategory.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i==0){
+                    spCategory.setLayoutParams(new Toolbar.LayoutParams(300, ViewGroup.LayoutParams.WRAP_CONTENT));
+                }else{
+                    spCategory.setLayoutParams(new Toolbar.LayoutParams(350, ViewGroup.LayoutParams.WRAP_CONTENT));
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(frame.getId(), MoviesFragment.newInstance(0,"Movies"),"Frag1");
